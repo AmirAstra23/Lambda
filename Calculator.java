682bbbc0437be892058a1b749f3e4d185428c965
AmirMurmansk <amobaybek@gmail.com>
2023-06-09 21:35:54 +0300

    Add files

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+
+public class Calculator implements Supplier {
+
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+    Calculator() { }
+    static Supplier instance = () -> new Calculator();
+    @Override
+    public Calculator get() {
+
+        return null;
+    }
+}
